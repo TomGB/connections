c857759974f4df700f447600890e14b84948dca7
TomGB <tgbanister@gmail.com>
2021-01-23 21:27:32 +0000

    various fixes

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,16 +1,7 @@
 import React from "react"
-import { Form, Input, Button } from "antd"
-import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+import { Form, Input, Button, Col, Row, Modal } from "antd"
+import { DeleteOutlined, PlusOutlined, CheckCircleOutlined } from '@ant-design/icons';
 import { compressToEncodedURIComponent as lzEncode } from 'lz-string'
-
-const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
-}
-
-const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
-}
 
 const FormList = ({ FormItem, AddButton }) => (
     <Form.List name="clues">
@@ -29,46 +20,82 @@ const FormList = ({ FormItem, AddButton }) => (
 
 const onFinish = (fields) => {
     const encoded = lzEncode(JSON.stringify(fields))
-    console.log(window.location.origin + process.env.PUBLIC_URL + '/#/play/' + encoded)
+
+    console.log(fields)
+    const gameUrl = window.location.origin + process.env.PUBLIC_URL + '/#/play/' + encoded
+
+    Modal.confirm({
+        title: 'Game successfully created',
+        content: (
+            <div>
+                <p>Open this link to play the game</p>
+                <a target='_blank' href={gameUrl}>{gameUrl}</a>
+            </div>
+        ),
+        icon: <CheckCircleOutlined style={{ color: '#52c41a' }} />,
+        maskClosable: true,
+        okText: 'Play Game',
+        cancelText: 'Close',
+        onOk() {
+            window.open(gameUrl, "_blank")
+        },
+        onCancel() { }
+    });
 }
+
+const LabelAndInput = ({ label = null, children }) => (
+    <Row style={{ marginLeft: '10px', marginRight: '20px' }}>
+        <Col span={8} style={{ textAlign: 'right', paddingRight: '10px', lineHeight: '32px' }}>{label}</Col>
+        <Col span={24 - 8}>{children}</Col>
+    </Row>
+)
+
 const Create = () => (
     <div style={{ maxWidth: '500px' }}>
-        <Form {...layout}
+        <Form
+            layout='horizontal'
             initialValues={{ clues: Array(12).fill("") }}
             onFinish={onFinish}
         >
-            <Form.Item label="Answer" name='answer'>
-                <Input placeholder="The connection" />
-            </Form.Item>
+            <LabelAndInput label="Answer:">
+                <Form.Item name='answer'>
+                    <Input placeholder="The connection" />
+                </Form.Item>
+            </LabelAndInput>
             <FormList
                 FormItem={({ field, index, remove }) => (
-                    <Form.Item
-                        {...field}
-                        name={field.name}
-                        fieldKey={field.fieldKey}
-                        label={index + 1}
-                        style={{ marginBottom: '8px' }}
-                    >
-                        <Input placeholder="Clue" suffix={
-                            <DeleteOutlined style={{ color: '#F66' }} onClick={() => remove(field.name)} />
-                        } />
-                    </Form.Item>
+                    <LabelAndInput label={index + 1 + ':'}>
+                        <Form.Item
+                            {...field}
+                            name={field.name}
+                            fieldKey={field.fieldKey}
+                            style={{ marginBottom: '8px' }}
+                        >
+                            <Input placeholder="Clue" suffix={
+                                <DeleteOutlined style={{ color: '#F66' }} onClick={() => remove(field.name)} />
+                            } />
+                        </Form.Item>
+                    </LabelAndInput>
                 )}
                 AddButton={({ add }) => (
-                    <Form.Item {...tailLayout}>
-                        <Button
-                            type="dashed"
-                            onClick={() => add()}
-                            block icon={<PlusOutlined />}
-                        >
-                            Add Clue
-                        </Button>
-                    </Form.Item>
+                    <LabelAndInput>
+                        <Form.Item>
+                            <Button
+                                type="dashed"
+                                onClick={() => add()}
+                                block icon={<PlusOutlined />}
+                            >
+                                Add Clue
+                            </Button>
+                        </Form.Item>
+                    </LabelAndInput>
                 )}
             />
-            <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit">Start</Button>
-            </Form.Item>
+            <LabelAndInput>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">Start</Button>
+                </Form.Item>
+            </LabelAndInput>
         </Form>
     </div>
 )
